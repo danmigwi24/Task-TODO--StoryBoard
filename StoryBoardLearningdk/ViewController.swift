aadae6cba07879fac51b810219f22c845a438737
DK <kimani.daniel@eclectics.io>
2024-03-16 18:33:20 +0300

    Initial Commit

---
+++ StoryBoardLearningdk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryBoardLearningdk
+//
+//  Created by Daniel Kimani on 16/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
